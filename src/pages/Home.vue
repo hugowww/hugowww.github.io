80b07227310c1489a04305b8a9a9e8f33f65c108
hugo <hugo572g@outlook.com>
2025-01-12 15:44:28 +0800

    1. move folder(projects) 2. wriet single and multi-threaded prime number calculation

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,18 +7,19 @@
       <router-link to="/minesweeper" class="btn btn-primary me-2"
         >踩地雷</router-link
       >
+      <router-link to="/snake" class="btn btn-success me-2">貪食蛇</router-link>
+      <router-link to="/find-prime" class="btn btn-warning me-2"
+        >找質數 (js worker)</router-link
+      >
     </div>
 
     <div class="flex-row border p-2 mt-2">
       <h2>Todo List</h2>
-      <router-link to="/snake" class="btn btn-success me-2"
-        >貪食蛇</router-link
-      >
-      <router-link to="/page-c" class="btn btn-info me-2"
-        >js worker</router-link
-      >
+
       <router-link to="/page-c" class="btn btn-info me-2">openGL</router-link>
-      <router-link to="/page-c" class="btn btn-info me-2">openGL-雪模擬</router-link>
+      <router-link to="/page-c" class="btn btn-info me-2"
+        >openGL-雪模擬</router-link
+      >
       <router-link to="/page-c" class="btn btn-info me-2">d3</router-link>
     </div>
   </div>
---
+++ src/projects/find_prime/core/IWorker.ts
@@ -0,0 +1,6 @@
+export interface IWorker {
+  /**
+   * Start the prime number calculation
+   */
+  start(): void;
+}
